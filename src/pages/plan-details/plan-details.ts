848fbdead1d75addf130f9e1c420114b9a78cfa8
Segun Konibire <gunzalez@hotmail.com>
2018-05-11 23:12:49 +0100

    removes dummy timestamp

--- src/pages/plan-details/plan-details.ts
+++ src/pages/plan-details/plan-details.ts
@@ -28,25 +28,9 @@ export class PlanDetailsPage {
     this.id = this.navParams.get('id');
   }
 
-  ionViewDidLoad() {
-
-    // just messing about with the date
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth()+1; //January is 0!
-    let yyyy = today.getFullYear();
-
-    if(dd<10){
-      dd = 0 + dd;
-    }
-    if(mm<10){
-      mm = 0 + mm;
-    }
-    this.created  = dd + '/' + mm + '/' + yyyy;
-  };
+  ionViewDidLoad() {};
 
   ionViewWillEnter(){
-
     let next = plan => {
       this.plan = plan;
       this.schedule = this.plan['participants'];
--- src/pages/plans/plans.ts
+++ src/pages/plans/plans.ts
@@ -25,29 +25,9 @@ export class PlansPage {
 
   @ViewChild('input') myInput;
 
+  ionViewDidLoad() {}
 
-
-  ionViewDidLoad() {
-
-    //just messing about with the date
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth()+1; //January is 0!
-    let yyyy = today.getFullYear();
-
-    if(dd<10){
-      dd = 0 + dd;
-    }
-    if(mm<10){
-      mm = 0 + mm;
-    }
-    this.created  = dd + '/' + mm + '/' + yyyy;
-
-  }
-
-  ionViewDidEnter(){
-    //
-  }
+  ionViewDidEnter(){}
 
   displayLoadingSpinner() {
     this.loading = this.loadingCtrl.create({
