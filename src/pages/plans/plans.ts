cdeaaa582683f64f38cdde16db57fec4e93063f5
Segun Konibire <gunzalez@hotmail.com>
2018-05-11 22:56:29 +0100

    updates keyboard native plugins

--- src/pages/plans/plans.ts
+++ src/pages/plans/plans.ts
@@ -134,6 +134,7 @@ export class PlansPage {
     this.addMode = mode;
     if (mode) {
       this.myInput.setFocus();
+      this.keyboard.hideKeyboardAccessoryBar(true);
       this.keyboard.show();
     } else {
       this.newPlanName = '';
