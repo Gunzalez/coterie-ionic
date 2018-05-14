463ebf5d3919d05142012424141bc1abf59f25bd
Segun Konibire <gunzalez@hotmail.com>
2018-05-14 20:19:55 +0100

    reworks plan screen

--- src/pages/plans/plans.ts
+++ src/pages/plans/plans.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
-import { Keyboard } from '@ionic-native/keyboard';
+// import { Keyboard } from '@ionic-native/keyboard';
 
 import { PlansProvider } from '../../providers/plans/plans';
 import { PlanDetailsPage } from '../plan-details/plan-details';
@@ -13,18 +13,15 @@ import { getIcon } from  '../../helpers/helpers';
 })
 export class PlansPage {
   public plans = [];
-  public created = "Monday";
-  public newPlanName = '';
+  public created:string = "Monday";
+  public newPlanName:string = '';
 
   private addMode: Boolean = false;
-  private loading = null;
-
-  constructor(private plansProvider: PlansProvider, public loadingCtrl: LoadingController, public navCtrl: NavController, private keyboard: Keyboard) {
-
-  }
+  private loading:any = null;
 
   @ViewChild('input') myInput;
 
+  constructor(private plansProvider: PlansProvider, public loadingCtrl: LoadingController, public navCtrl: NavController) {}
 
 
   ionViewDidLoad() {
@@ -43,40 +40,38 @@ export class PlansPage {
     }
     this.created  = dd + '/' + mm + '/' + yyyy;
 
+
+
   }
 
-  ionViewDidEnter(){
-    //
+  allowedAction(){
+    return this.newPlanName.length ? "Save" : "New"
   }
 
   displayLoadingSpinner() {
     this.loading = this.loadingCtrl.create({
       content: 'Please wait...'
     });
-
     this.loading.present();
-
   }
 
   ionViewWillEnter() {
+    this.newPlanName = '';
+    this.addMode = false;
     this.displayLoadingSpinner();
     this.getAllPlans();
   }
 
-  ionViewDidLeave(){
-    this.setPlanMode(false);
+  getAllPlans(){
+    let next = data => {
+      this.plans = data.plans.reverse();
+      this.loading.dismiss();
+    };
+    this.plansProvider.getPlans().subscribe(next);
   }
 
   isInAddMode(){
     return this.addMode;
-  }
-
-  getAllPlans(){
-    let next = data => {
-      this.plans = data.plans;
-      this.loading.dismiss();
-    };
-    this.plansProvider.getPlans().subscribe(next);
   }
 
   getPlanIcon(plan){
@@ -91,14 +86,15 @@ export class PlansPage {
     if(this.canBeDeleted(plan)){
       let next = response => {
         if(response){
+          this.displayLoadingSpinner();
           this.getAllPlans();
         }
       };
       this.plansProvider.deletePlan(plan['id']).subscribe(next);
     } else {
       // console.log('Archive')
+      console.log('Snoo[')
     }
-
   }
 
   getIconColour(plan){
@@ -119,51 +115,30 @@ export class PlansPage {
   }
 
   addButtonClicked(){
-    if(!this.isInAddMode()){
-      this.setPlanMode(true)
-    }  else {
-      this.addPlan();
-    }
-  }
-
-  resignFocus(){
-    //this.setPlanMode(false);
-  }
-
-  setPlanMode(mode){
-    this.addMode = mode;
-    if (mode) {
-      this.myInput.setFocus();
-      this.keyboard.show();
-    } else {
-      this.newPlanName = '';
-      this.keyboard.close();
-    }
-  }
-
-  addPlan(){
-
-    let planName = this.newPlanName.trim();
-    if(planName.length){
-
+    if(this.newPlanName.trim().length){
+      let planName = this.newPlanName.trim();
       let next = data => {
+        this.newPlanName = '';
         let pathArr = data.headers.get('location').split('/');
         let id = pathArr[pathArr.length - 1];
         let params = { id: id };
         this.navCtrl.push(PlanDetailsPage, params);
-        // this.setPlanMode(false);
       };
       this.plansProvider.addPlan(planName).subscribe(next);
-
     } else {
-      this.setPlanMode(false);
+      this.addMode = true;
+      this.myInput.setFocus();
     }
+  }
+
+  resignFocus(){
+    this.addMode = false;
+    this.newPlanName = '';
   }
 
   viewPlan(plan){
     let params = { id: plan.id };
     this.navCtrl.push(PlanDetailsPage, params);
-    // this.setPlanMode(false);
   }
 
 }
